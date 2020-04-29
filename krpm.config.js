b66d861c6c49113a2cb03f80e4b996e96eb56dd1
karikera <karikera3@hotmail.com>
2020-04-30 04:35:25 +0900

    visual studio 2019 build krpm

---
+++ krpm.config.js
@@ -0,0 +1,30 @@
+
+module.exports = {
+	platforms: ['x86', 'x64'],
+	static: true,
+	files: [
+		['README'],
+		['include', 'include/mysql', [
+			'mysql.h', 
+			'errmsg.h',
+			'mariadb_com.h',
+			'ma_list.h',
+			'mariadb_ctype.h',
+			'mariadb_stmt.h',
+		]],
+		['build_x86/include', 'include/mysql', [
+			'mariadb_version.h', 
+		]],
+	],
+	each(krb)
+	{
+		krb.vsbuild(`build_${krb.platform.shortName}/mariadb-connector-c.sln`, krb.config.name, krb.platform.longName);
+		krb.copy(
+			`build_${krb.platform.shortName}/libmariadb/${krb.config.name}`,
+			`${krb.libExportDir}/${krb.config.name}`, [
+				'libmariadb.dll',
+				'libmariadb.lib',
+				'mariadbclient.lib',
+			]);
+	}
+};
